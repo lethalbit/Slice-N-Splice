0052acd7488cc0af72f7c3167ea5885a5372ca59
Aki Van Ness <aki@lethalbit.net>
2019-08-17 21:19:47 -0400

    Fixed mkobj.sh so that if it the target directory doesn't exist it will create it

--- etc/mkobj.sh
+++ etc/mkobj.sh
@@ -3,6 +3,10 @@ if [ $# -lt 2 ]; then
 	echo "usage: $0 ASM OBJ <GCC OPTS>"
 	exit;
 fi
+
+OUT_DIR=${2%/*}
+[ ! -d $OUT_DIR ] && mkdir -p $OUT_DIR
+
 TMP_OBJ="$(mktemp -u)"
 gcc -m64 -c $1 -o $TMP_OBJ
 ld --oformat binary -o $2 $TMP_OBJ
