5cdfe5a160c78cce6957d3b78261619b9ad91c0a
Aki Van Ness <aki@lethalbit.net>
2019-08-17 21:00:23 -0400

    Added new test files for generation

---
+++ etc/mkobj.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+	echo "usage: $0 ASM OBJ <GCC OPTS>"
+	exit;
+fi
+TMP_OBJ="$(mktemp -u)"
+gcc -m64 -c $1 -o $TMP_OBJ
+ld --oformat binary -o $2 $TMP_OBJ
+rm $TMP_OBJ
