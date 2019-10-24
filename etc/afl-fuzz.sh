d3c2feb958ab2c6d546b245c943a42593a4fbe15
Aki Van Ness <aki@lethalbit.net>
2019-10-23 21:42:25 -0400

    Added the AFL fuzzing script and some initial content to the fuzzing harness

--- etc/afl-fuzz.sh
+++ etc/afl-fuzz.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
-echo "Fuzzing $1"
+AFL_DIR=$MESON_BUILD_ROOT/afl
+AFL_SYNC_DIR=$AFL_DIR/afl_sync
+AFL_TESTCASE_DIR=$AFL_DIR/afl_test
+
+if [ ! -d $AFL_DIR  ]; then
+		mkdir -p $AFL_DIR $AFL_SYNC_DIR $AFL_TESTCASE_DIR
+fi
+echo "Copying initial test case"
+cp $MESON_BUILD_ROOT/test-objs/* $AFL_TESTCASE_DIR/
+
+CPU_COUNT=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | tail -1)
+echo "Found $CPU_COUNT CPU Cores"
+
+echo "Tweaking System, I'mma need root for that"
+sudo -v
+
+echo "Setting core dump pattern"
+echo core | sudo tee /proc/sys/kernel/core_pattern
+
+for i in $(seq 0 $CPU_COUNT); do
+	echo "Setting scaling_governor to \"performance\" for CPU $i"
+	TGT_CPU="cpu$i"
+	echo performance | sudo tee /sys/devices/system/cpu/$TGT_CPU/cpufreq/scaling_governor
+done
+
+echo "Dropping root, peace~"
+sudo -k
+
+echo "Starting AFL Master"
+afl-fuzz -i $AFL_TESTCASE_DIR -o $AFL_SYNC_DIR -M fuzzer1 $1 @@ 1>/dev/null&
+
+
+for i in $(seq 1 $(($CPU_COUNT-1))); do
+	SLAVE_NAME="fuzz-slave-$i"
+	echo "Spawning AFL slave $SLAVE_NAME"
+	afl-fuzz -i $AFL_TESTCASE_DIR -o $AFL_SYNC_DIR -S $SLAVE_NAME $1 @@ 1>/dev/null&
+done
+echo "Fuzzers now running...."
--- src/fuzz-harness/afl-fuzzer.cc
+++ src/fuzz-harness/afl-fuzzer.cc
@@ -1,4 +1,21 @@
 /* SNS AFL fuzzer harness */
 
+#include <zlib.hh>
+#include <utility.hh>
 
-int main() { return 0; }
+#if defined(_fuzz_target_elf)
+#include <elf.hh>
+#endif
+
+int main(int argc, char** argv) {
+
+
+#if defined(_fuzz_target_elf)
+	elf64_t elf{argv[1]};
+
+
+
+#endif
+
+	return 0;
+}
