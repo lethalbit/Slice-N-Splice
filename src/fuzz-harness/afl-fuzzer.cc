1f26a6602b061944e81ba7cccf8174827c72665e
Aki Van Ness <aki@lethalbit.net>
2019-11-01 01:19:59 -0400

    Updated the elf-fuzz target a little bit to attempt to get more coverage.

--- src/fuzz-harness/afl-fuzzer.cc
+++ src/fuzz-harness/afl-fuzzer.cc
@@ -13,8 +13,16 @@ int main(int argc, char** argv) {
 
 #if defined(_fuzz_target_elf)
 	elf64_t elf{argv[1]};
-
-
+	std::cout << "CLASS: " << elf.header().ident().eclass() << "\n";
+	std::cout << "DATA: " << elf.header().ident().data() << "\n";
+	std::cout << "VERSION: " << elf.header().ident().version() << "\n";
+	std::cout << "ABI: " << elf.header().ident().abi() << "\n";
+	std::cout << "ABI VERSION: " << elf.header().ident().abi_version() << "\n";
+	std::cout << "TYPE: " << elf.header().type() << "\n";
+	std::cout << "MACHINE: " << elf.header().machine() << "\n";
+	std::cout << "VERSION: " << elf.header().version() << "\n";
+	std::cout << "ENTRY: " << elf.header().entry() << "\n";
+	std::cout << "PHOFF: " << elf.header().phoff() << "\n";
 
 #endif
 
